bae5c11d1aa53112a71f6d6c99ec0a25cf1a1d80
SnAzZyC00kie <SnAzZyC00kie@users.noreply.github.com>
2019-02-13 15:29:48 -0400

    RaNdOm NuMbeR Generator

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,16 @@ using namespace std;
 int main()
 {
     int seed = -1, n = -1;
-    string bases = "";
+    string bases =" "; // Segmentation core dump error
 
 
-    cout<<"\nWelcome to the DNA generator\n\n\tPlease enter a seed, the letter bases to be used (e.g. TGAC) \n\tand the length of the DNA sequence to be generated\n\n\t>>> ";   
+    cout<<"\nWelcome to the DNA generator\n\n\tPlease enter a seed, the letter bases to be used (e.g. TGAC) \n\tand the length of the DNA sequence to be generated.\n\n\t>>> ";   
 
-    cin>>seed>>bases>>n;//read the three inputs
+    cin>>seed;
+    cout<< "\n\t>>> ";
+    cin>>bases;
+    cout<< "\n\t>>>";
+    cin>>n;//read the three inputs
 
     cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n)<<endl; //print output  
 }
