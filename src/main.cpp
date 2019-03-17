7a07499d1cd90ab7a69f26310d9a38a22b3a28e5
SnAzZyC00kie <SnAzZyC00kie@users.noreply.github.com>
2019-03-17 19:09:46 -0400

    rANdOM nUMbeR GEnEraTor

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,15 @@ using namespace std;
 int main()
 {
     int seed = -1, n = -1;
-    string bases ="'|\"<[)`=%^?/#_Q-@Y.UFGCIONMBEAxvwy895kb3d2tnioaers1lm0phu4g67fjzqSRTLPDHKJW!*VZX$,+; ~&\\]:(>{}";
-    // Segmentation core dump error
-    int mean;
-    float std;
-    int num_guesses;
-
+    string bases =""; // Segmentation core dump error
 
     cout<<"\nWelcome to the DNA generator\n\n\tPlease enter a seed, the letter bases to be used (e.g. TGAC) \n\tand the length of the DNA sequence to be generated.\n\n\t>>> ";   
-
     cin>>seed;
     cout<< "\n\t>>>";
-    cin>>n;//read the three inputs
+    cin>>bases;//read the three inputs
     cout<< "\n\t>>>";
-    cin >>mean;
-     cout<< "\n\t>>>";
-     cin>>std;
-     cout<< "\n\t>>>";
-     cin>>num_guesses;
+    cin>>n;//read the three inputs
 
-    cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n,mean,std,num_guesses)<<endl; //print output  
+    cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n)<<endl; //print output  
 }
 
